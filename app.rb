5c07081011231ef1b061bef0c6446410aa272119
Jack Sarick <jacksarick@gmail.com>
2016-11-16 23:28:43 -0500

    Basically setup to make anything I want

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 require "socket"
 
+PAGE_ROOT = "./pages/"
+
 server = TCPServer.new('localhost', 3000)
 print "server @ http://localhost:3000\n"
+
+def compose(filename, replacements = nil)
+	file = File.read(PAGE_ROOT + filename.strip)
+	file = file.gsub(/<<<([^>]+)>>>/) {|include_file| compose($1)}
+
+	if replacements != nil
+		file = file.gsub(/\{\{\{([^\}]+)\}\}\}/) {|variable| replacements[variable]}
+	end
+
+	return file
+end
 
 def http_response(response)
 	return "HTTP/1.1 200 OK\r\n" +
@@ -18,11 +31,12 @@ loop do
 	request = socket.gets.split
 
 	print request
+	print "\n"
 
 	if request[0] == "GET"
 		case request[1]
 		when "/"
-			socket.print http_response File.read("pages/index.html")
+			socket.print http_response compose("index.html")
 		else
 			socket.print http_response "You asked for #{request[1]}"
 		end
